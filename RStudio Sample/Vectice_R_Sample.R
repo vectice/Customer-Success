61dff2bb08b1a01aec6cddae576416b646b970d6
eric-barre <barre.eric@gmail.com>
2023-06-07 12:00:32 -0400

    Added model code and comments

--- RStudio Sample/Vectice_R_Sample.R
+++ RStudio Sample/Vectice_R_Sample.R
@@ -17,14 +17,11 @@ pty_id_raw <- read.csv("RStudio Sample/PTY_ID_MAIN.csv", header=TRUE)
 pty_id_rmhead <- subset(pty_id_raw,Customer_State_Address != 'Customer_State_Address')
 pty_id_noCA <- subset(pty_id_rmhead,Customer_State_Address != 'CA')
 
-#Drop Customer_Zip_Address variable to avoid bias trap and Customer_SSN as it is a PII variable
-pty_id_clean <- subset(pty_id_noCA, select = -c(Customer_Zip_Address, Customer_SSN))
+#Drop Customer_Zip_Address variable to avoid bias trap and Customer_SSN and Customer_Email as they are PII variables
+pty_id_clean <- subset(pty_id_noCA, select = -c(Customer_Zip_Address, Customer_SSN, Customer_Email))
 
 # Save cleaned dataset locally
 write.csv(pty_id_clean, "RStudio Sample/PTY_ID_CLEAN.csv", row.names=FALSE)
-
-#pty_id_clean %>%
-#  View()
 
 # Basic Data Visualization
 
@@ -45,18 +42,65 @@ dev.off()
 #######
 
 
-# VECTICE DOCUMENTATION PIPELINE
-vectice <- reticulate::import("vectice")
-pd <- reticulate::import("pandas")
+#######
+#
+# EXAMPLE BUILD MODEL
+#
+#######
 
+library(caret)
+library(dplyr)
+library(randomForest)
+library(gridExtra)
+
+df_stroke<-read.csv("RStudio Sample/healthcare-dataset-stroke-data.csv", header = TRUE)
+# Drop the column with 'other'.(Since there is only 1 row)
+df_stroke = df_stroke[!df_stroke$gender == 'Other',]
+#imputing dataset
+df_stroke$bmi[is.na(df_stroke$bmi)]<- mean(df_stroke$bmi,na.rm = TRUE)
+
+df_stroke$stroke<- factor(df_stroke$stroke, levels = c(0,1), labels = c("No", "Yes"))
+df_stroke$gender<-as.factor(df_stroke$gender)
+df_stroke$hypertension<- factor(df_stroke$hypertension, levels = c(0,1), labels = c("No", "Yes"))
+df_stroke$heart_disease<- factor(df_stroke$heart_disease, levels = c(0,1), labels = c("No", "Yes"))
+df_stroke$ever_married<-as.factor(df_stroke$ever_married)
+df_stroke$work_type<-as.factor(df_stroke$work_type)
+df_stroke$Residence_type<-as.factor(df_stroke$Residence_type)
+df_stroke$smoking_status<-as.factor(df_stroke$smoking_status)
+df_stroke$bmi<-as.numeric(df_stroke$bmi)
+
+#Lets split the final dataset to training and test data
+n_obs <- nrow(df_stroke)
+split <- round(n_obs * 0.7)
+train <- df_stroke[1:split,]
+# Create test
+test <- df_stroke[(split + 1):nrow(df_stroke),]
+write.csv(train, "RStudio Sample/train.csv", row.names=FALSE)
+write.csv(test, "RStudio Sample/test.csv", row.names=FALSE)
+
+#Modeling
+set.seed(123)
+rf_model<-randomForest(formula= stroke~.,data = train, metric='Accuracy', na.action=na.roughfix)
+
+cm <- confusionMatrix(predict(rf_model, test), test$stroke)
+str(cm)
+
+
+
+######
+#
+# VECTICE DOCUMENTATION PIPELINE
+#
+######
+
+vectice <- reticulate::import("vectice")
 # Connect to Vectice
-vct <- vectice$connect(api_token = 'ynqdogKBN.6mDRJXaMkGjrobPx0LwvEynqdogKBNl69e8VYZO2dQ3WA47pgz')
+vct <- vectice$connect(config = 'vect_config.json')
 
 #Catalog the raw data
 #Start a new iteration
-phase <- vct$phase("PHA-1280")$create_iteration()
-pdDF <- reticulate::r_to_py(pty_id_raw)
-ds_resource <- vectice$FileResource(paths="RStudio Sample/PTY_ID_MAIN.csv", dataframes = pdDF)
+phase <- vct$phase("PHA-1301")$create_iteration()
+ds_resource <- vectice$FileResource(paths="RStudio Sample/PTY_ID_MAIN.csv", dataframes = pty_id_rmhead)
 raw_ds <- vectice$Dataset$origin(name="PTY_ID_MAIN", resource=ds_resource)
 phase$step_identify_data <- raw_ds
 phase$step_describe <- "PTY_ID_MAIN is the source data for this project"
@@ -64,10 +108,24 @@ phase$complete()
 
 #Catalog the cleaned data
 #Start a new iteration
-phase <- vct$phase("PHA-1281")$create_iteration()
-pdDF <- reticulate::r_to_py(pty_id_clean)
-ds_resource <- vectice$FileResource(paths="RStudio Sample/PTY_ID_CLEAN.csv", dataframes = pdDF)
-clean_ds <- vectice$Dataset$clean(name="PTY_ID_CLEAN", resource=ds_resource, attachments = "RStudio Sample/pty_id_states.jpg")
+phase <- vct$phase("PHA-1302")$create_iteration()
+ds_resource <- vectice$FileResource(paths="RStudio Sample/PTY_ID_CLEAN.csv", dataframes = pty_id_clean)
+clean_ds <- vectice$Dataset$clean(name="PTY_ID_CLEAN", resource=ds_resource, attachments = "RStudio Sample/pty_id_states.jpg", derived_from = raw_ds$latest_version_id)
 phase$step_clean_data <- clean_ds
 phase$complete()
+
+#Catalog the modeling dataset
+phase <- vct$phase("PHA-1300")$create_iteration()
+ds_train <- vectice$FileResource(paths="RStudio Sample/train.csv", dataframes = train)
+ds_test <- vectice$FileResource(paths="RStudio Sample/test.csv", dataframes = test)
+modeling_ds <- vectice$Dataset$modeling(name="Modeling_Dataset", training_resource=ds_train, testing_resource=ds_test, derived_from = clean_ds$latest_version_id)
+phase$step_modeling_dataset <- modeling_ds
+
+#Catalog the model itself
+stats <- list("Accuracy" = cm$overall[1], "McnemarPValue" = cm$overall[7])
+model <- vectice$Model(name = "Predictor model", library = "scikit-learn", technique = "randomForest", metrics = stats, attachments = "RStudio Sample/regression_graph.png", derived_from  = modeling_ds$latest_version_id)
+phase$step_build_model <- model
+phase$complete()
+
+
 
