d64c22d4bfb80f017452caef87597249c9b5da88
eric-barre <barre.eric@gmail.com>
2023-10-17 14:26:22 -0700

    Updated R sample with new syntax

--- RStudio Sample/Vectice_R_Sample.R
+++ RStudio Sample/Vectice_R_Sample.R
@@ -33,7 +33,6 @@ df_stroke_clean$bmi<-as.numeric(df_stroke_clean$bmi)
 
 write.csv(df_stroke_clean, "healthcare-dataset-stroke-data-clean.csv", row.names=FALSE)
 
-
 #Lets split the final dataset to training and test data
 n_obs <- nrow(df_stroke_clean)
 split <- round(n_obs * 0.7)
